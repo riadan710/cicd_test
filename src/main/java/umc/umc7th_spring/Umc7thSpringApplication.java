b5c13483aef92c3a712e4729c3d4a5f394c60b5a
riadan710 <riadan710@gmail.com>
2024-11-11 16:15:49 +0900

    initial setting

---
+++ src/main/java/umc/umc7th_spring/Umc7thSpringApplication.java
@@ -0,0 +1,13 @@
+package umc.umc7th_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc7thSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc7thSpringApplication.class, args);
+	}
+
+}
