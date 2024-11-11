ff45b7b283d65a19ebbce015678e63bb32fe9341
riadan710 <riadan710@gmail.com>
2024-11-11 20:54:10 +0900

    [Refactor] 5주차 미션 어노테이션 수정

--- src/main/java/umc/umc7th_spring/domain/Mission.java
+++ src/main/java/umc/umc7th_spring/domain/Mission.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Mission extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Integer reward;
--- src/main/java/umc/umc7th_spring/domain/Region.java
+++ src/main/java/umc/umc7th_spring/domain/Region.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Region extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
