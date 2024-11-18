562d2d3559e14548866ff71e2df18e1b54a46dea
riadan710 <riadan710@gmail.com>
2024-11-18 12:40:07 +0900

    [Feat] 7주차 실습 에러핸들링 구현

---
+++ src/main/java/umc/umc7th_spring/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package umc.umc7th_spring.apiPayload.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import umc.umc7th_spring.apiPayload.code.BaseErrorCode;
+import umc.umc7th_spring.apiPayload.code.ErrorReasonDTO;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
