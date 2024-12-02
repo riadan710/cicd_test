528c0c7ff0f4f4c83a4a84b43c095ed54942ff29
riadan710 <riadan710@gmail.com>
2024-12-02 18:52:31 +0900

    [Feat] 10주차 실습

--- src/main/java/umc/umc7th_spring/config/security/SecurityConfig.java
+++ src/main/java/umc/umc7th_spring/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +16,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable) // 추가해주어야함.
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/signup", "/members/signup", "/css/**").permitAll()
+                        .requestMatchers("/", "/home", "/signup", "/members/signup", "/css/**", "/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
--- src/main/java/umc/umc7th_spring/web/controller/MemberViewController.java
+++ src/main/java/umc/umc7th_spring/web/controller/MemberViewController.java
@@ -19,9 +19,11 @@ public class MemberViewController {
     // thymeleaf 사용을 위해 일부가 변경되었습니다.
     // 실제로는 8주차에서 작성한 컨트롤러와 동일하게 작성하시면 됩니다!!
     @PostMapping("/members/signup")
-    public String joinMember(@ModelAttribute("memberJoinDto") MemberRequestDTO.JoinDTO request, // 협업시에는 기존 RequestBody 어노테이션을 붙여주시면 됩니다!
+    public String joinMember(@ModelAttribute("memberJoinDTO") MemberRequestDTO.JoinDTO request, // 협업시에는 기존 RequestBody 어노테이션을 붙여주시면 됩니다!
                              BindingResult bindingResult,
                              Model model) {
+        System.out.println(request.getAddress());
+
         if (bindingResult.hasErrors()) {
             // 뷰에 데이터 바인딩이 실패할 경우 signup 페이지를 유지합니다.
             return "signup";
@@ -44,7 +46,7 @@ public class MemberViewController {
 
     @GetMapping("/signup")
     public String signupPage(Model model) {
-        model.addAttribute("memberJoinDto", new MemberRequestDTO.JoinDTO());
+        model.addAttribute("memberJoinDTO", new MemberRequestDTO.JoinDTO());
         return "signup";
     }
 
